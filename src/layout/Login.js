9ecbf795a04774cbdb6edf693b04f21ea1a386f8
Maia-Michel <contateste.m8529919@gmail.com>
2022-05-18 16:01:21 -0300

    Aula1 conceitos sobre react, desenvolvendo os primeiros passos do formulario

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import logo from '../assets/images/logo.svg';
 import './App.css';
 
-function App() {
+
+function Login() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login 
         </p>
         <a
           className="App-link"
@@ -22,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Login;
